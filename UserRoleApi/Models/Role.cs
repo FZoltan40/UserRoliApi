c7b3f8188c70a08a439dcb9ca4552d048c9e05fc
FZoltan40 <farkasz@kkszki.hu>
2025-12-01 08:41:44 +0100

    Models and migrations are successfull created.

---
+++ UserRoleApi/Models/Role.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserRoleApi.Models
+{
+    public class Role
+    {
+        public Guid Id { get; set; }
+        [Column(TypeName = "varchar(30)")]
+        public string Name { get; set; }
+        public DateTime RegTime { get; set; }
+
+        //Kapcsolatok
+        public ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
+    }
+}
---
+++ UserRoleApi/Models/UserDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserRoleApi.Models
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=authuser;user=root;password=");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserRole>()
+                .HasKey(ru => new { ru.UserId, ru.RoleId });
+
+            modelBuilder.Entity<UserRole>()
+                .HasOne(ru => ru.User)
+                .WithMany(u => u.UserRoles)
+                .HasForeignKey(ru => ru.UserId);
+
+            modelBuilder.Entity<UserRole>()
+                .HasOne(ru => ru.Role)
+                .WithMany(r => r.UserRoles)
+                .HasForeignKey(ru => ru.RoleId);
+        }
+    }
+
+}
+
